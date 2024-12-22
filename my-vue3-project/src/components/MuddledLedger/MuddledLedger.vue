4e3d5967b9f631de41f97574469c145f191a854e
linyujia <3180057615@qq.com>
2024-12-23 00:09:42 +0800

    糊涂啊

---
+++ my-vue3-project/src/components/MuddledLedger/MuddledLedger.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import {ref} from "vue";
+import LedgerTab from "@/components/MuddledLedger/LedgerTab.vue";
+import Consumption from "@/components/MuddledLedger/Consumption.vue";
+import Category from "@/components/MuddledLedger/Category.vue";
+import Record from "@/components/MuddledLedger/Record.vue";
+import Report from "@/components/MuddledLedger/Report.vue";
+
+const cur_page = ref("consumption");
+function set_cur_page(val) {
+  cur_page.value = val
+}
+</script>
+
+<template>
+  <div>
+    <LedgerTab
+        @consumption="set_cur_page('consumption')"
+        @note="set_cur_page('note')"
+        @classify="set_cur_page('classify')"
+        @report="set_cur_page('report')"
+        @setting="set_cur_page('setting')"
+    ></LedgerTab>
+  </div>
+  <div>
+    <Consumption v-if="cur_page==='consumption'"></Consumption>
+    <Category  v-else-if="cur_page==='classify'"></Category>
+    <Record  v-else-if="cur_page==='note'"></Record>
+    <Report  v-else-if="cur_page==='report'"></Report>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-vue3-project/src/request/myrequest.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+axios.defaults.withCredentials=true;
+export const myHttp=axios.create({
+    withCredentials: true,
+    // 直接访问现网环境的后端服务，有跨域问题
+    // baseURL: "http://47.109.79.50:9802/api",
+    // 本地直接访问后端服务，有跨域问题
+    // baseURL: "http://127.0.0.1:9802/api",
+    // 部署现网环境nginx代理使用
+    // baseURL: "http://47.109.79.50:9898/api",
+    // 本地nginx代理使用
+    baseURL: process.env.VUE_APP_API_URL + '/api'
+    ,
+})
